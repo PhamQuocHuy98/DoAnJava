23e518a6ec7f3c4ad48e44f2e7f9c13e51f54cb5
Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2020-05-28 15:24:14 +0700

    init project

---
+++ src/com/nhom4/ui/MainScreen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nhom4.ui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author phamquochuy
+ */
+public class MainScreen {
+    
+    public static void main (String [] args){
+        new DashboardScreen().setVisible(true);
+    }
+}
